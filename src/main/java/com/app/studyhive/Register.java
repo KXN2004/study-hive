754d25571adc4d45f8cf54c2eb502d8beb3286f4
Kevin Nadar <kevinxaviernadar@gmail.com>
2023-10-18 09:06:13 +0530

    Connected Authorization to ToDo List

--- src/main/java/com/app/studyhive/Register.java
+++ src/main/java/com/app/studyhive/Register.java
@@ -3,18 +3,31 @@ package com.app.studyhive;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.sql.*;
+import java.util.Objects;
 
-import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import io.github.palexdev.materialfx.css.themes.MFXThemeManager;
+import io.github.palexdev.materialfx.css.themes.Themes;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
+
 public class Register {
+
+    private final Paint green = new Color(0, 1, 0, 1);
+
     @FXML
     private MFXDatePicker birthdate;
 
@@ -59,41 +72,52 @@ public class Register {
 
     boolean verifyInput(String string) {
         if (string.isEmpty()) {
-            prompt.setText("Some field is empty!");
+            prompt.setText("Some text field is empty!");
             return false;
         } else if (string.contains(" ")) {
-            prompt.setText("Some field contains whitespace!");
+            prompt.setText("Some text field contains whitespace!");
             return false;
         } else if (string.length() > 9) {
-            System.out.println(string.length());
-            prompt.setText("Some field entry is too long!");
+            prompt.setText("Some text field entry is too long!");
             return false;
         }
         return true;
     }
 
     boolean comparePasswords(String pass1, String pass2) {
-        System.out.println(pass1 + " " + pass2);
         if (!pass1.equals(pass2))
             prompt.setText("Passwords don't match");
         return pass1.equals(pass2);
     }
 
-    @FXML
-    void oauth(ActionEvent event) throws GeneralSecurityException, IOException {
-        Google.signIn();
+    void switchScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("todo-list.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
+        stage.setTitle("ToDo List");
+        stage.show();
     }
 
     @FXML
-    void signIn(ActionEvent event) throws SQLException {
+    void oauth(ActionEvent event) throws GeneralSecurityException, IOException {
+        Google.signIn();
+        switchScene(event);
+    }
+
+    @FXML
+    void signIn(ActionEvent event) throws SQLException, IOException {
         if (verifyInput(username.getText()) && verifyInput(password.getText())) {
             ResultSet resultSet = con.createStatement().executeQuery("select * from StudyHive.users where username = \"" + username.getText() + "\"");
             if (!resultSet.next()) {
                 prompt.setText("Username doesn't exist");
             } else {
-                if (BCrypt.checkpw(password.getText(), resultSet.getString("Password")))
+                if (BCrypt.checkpw(password.getText(), resultSet.getString("Password"))) {
+                    prompt.setTextFill(green);
                     prompt.setText("Login successful!");
-                else
+                    switchScene(event);
+                } else
                     prompt.setText("Incorrect password");
             }
         }
@@ -101,28 +125,33 @@ public class Register {
 
     @FXML
     void signUp(ActionEvent event) throws SQLException {
-        boolean proceed = verifyInput(username.getText())
+        if (!(verifyInput(username.getText())
             && verifyInput(password.getText())
             && verifyInput(fname.getText())
             && verifyInput(lname.getText())
             && verifyInput(email.getText())
-            && verifyInput(confirmPassword.getText());
-        boolean match = comparePasswords(password.getText(), confirmPassword.getText());
-        if (proceed && match) {
+            && verifyInput(confirmPassword.getText()))) {
+            return;
+        } else if (gender.getSelectedItem() == null) {
+            prompt.setText("Select your gender");
+            return;
+        } else if (birthdate.getValue() == null) {
+            prompt.setText("Select your birthdate");
+            return;
+        }
+        if (comparePasswords(password.getText(), confirmPassword.getText())) {
             try {
-                String hash = BCrypt.hashpw(password.getText(), BCrypt.gensalt(16));
                 String query = String.format(
                         "insert into StudyHive.users values (\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\")",
                         username.getText(),
                         fname.getText(),
                         lname.getText(),
                         gender.getSelectedItem(),
-                        birthdate.getSelectedText(),
+                        birthdate.getValue(),
                         email.getText(),
-                        hash
+                        BCrypt.hashpw(password.getText(), BCrypt.gensalt())
                 );
                 con.createStatement().execute(query);
-                prompt.setText("Registration successful!");
             } catch (SQLIntegrityConstraintViolationException ignore) {
                 prompt.setText("Student already exists!");
             }
